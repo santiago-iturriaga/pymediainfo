3a4cf41c8bfb535a372db490bd8788429e11cb58
Santiago <Santiago@63a0f744-5f35-11de-bc6d-35d2ebd5fd4e>
2009-06-26 22:15:02 +0000

    Delete bin, Fix return, DEBUG/RELEASE

--- trunk/pymediafinder/wrapper.cpp
+++ trunk/pymediafinder/wrapper.cpp
@@ -19,7 +19,18 @@ static PyObject* pymediafinder_find(PyObject *self, PyObject *args)
 	list<MediaInfo> result;
 	result = Finder::Find();
 
-	Py_RETURN_NONE;
+	list<MediaInfo>::iterator iter = result.begin();
+
+	PyObject *list = PyList_New(result.size());
+	int pos = 0;
+	while (iter != result.end()) {
+		MediaInfo current = *iter;
+		PyObject *tuple = Py_BuildValue("(ssss)", current.device_name.c_str(), current.mount_path.c_str(), current.format_type.c_str(), current.label.c_str());
+		PyList_SetItem(list, pos, tuple);
+	}
+
+	//Py_RETURN_NONE;
+	return list;
 }
 
 static PyMethodDef PyMediaFinderMethods[] = {
