593f76f43513ae419bde6374ee2cd0239d2d6318
Santiago <Santiago@63a0f744-5f35-11de-bc6d-35d2ebd5fd4e>
2009-06-26 22:44:17 +0000

    Fix return List of Tuples to Python

--- pymediafinder/wrapper.cpp
+++ pymediafinder/wrapper.cpp
@@ -19,18 +19,22 @@ static PyObject* pymediafinder_find(PyObject *self, PyObject *args)
 	list<MediaInfo> result;
 	result = Finder::Find();
 
-	list<MediaInfo>::iterator iter = result.begin();
+	PyObject *py_result = PyList_New(result.size());
 
-	PyObject *list = PyList_New(result.size());
+	list<MediaInfo>::iterator iter = result.begin();
 	int pos = 0;
+
 	while (iter != result.end()) {
 		MediaInfo current = *iter;
 		PyObject *tuple = Py_BuildValue("(ssss)", current.device_name.c_str(), current.mount_path.c_str(), current.format_type.c_str(), current.label.c_str());
-		PyList_SetItem(list, pos, tuple);
+		PyList_SetItem(py_result, pos, tuple);
+
+		pos++;
+		iter++;
 	}
 
 	//Py_RETURN_NONE;
-	return list;
+	return py_result;
 }
 
 static PyMethodDef PyMediaFinderMethods[] = {
